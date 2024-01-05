301044aafeb981f9c794d4ece3a053f6348b8a2b
Pengyong <zpyseagull@163.com>
2024-01-05 12:36:42 -0500

    static root

--- awesome_tutoring/settings.py
+++ awesome_tutoring/settings.py
@@ -127,20 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/' 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
-if DEBUG:
-  STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-  ]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if DEBUG:
+#   STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+#   ]
+# else:
+#   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -165,4 +165,5 @@ else:
 
 ALLOWED_HOSTS = [
     'pumpking-tutoring.onrender.com',
+    '127.0.0.1'
 ]
