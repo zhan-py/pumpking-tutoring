6da16f5a0fa02250dea2bd7c6d557b9fce15a558
Pengyong <zpyseagull@163.com>
2024-01-05 12:45:37 -0500

    static root

--- awesome_tutoring/settings.py
+++ awesome_tutoring/settings.py
@@ -129,17 +129,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
-# if DEBUG:
-#   STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-#   ]
-# else:
-#   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+  STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+  ]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
