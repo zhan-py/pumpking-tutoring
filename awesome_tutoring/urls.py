7592349687903ab36af562b93721199d08dc5f08
Pengyong <zpyseagull@163.com>
2024-01-05 13:33:12 -0500

    url with static file

--- awesome_tutoring/urls.py
+++ awesome_tutoring/urls.py
@@ -35,3 +35,4 @@ from . import settings
 from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
